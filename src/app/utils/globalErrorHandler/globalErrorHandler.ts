356bafa5547288b683c3077a4b3d02bd0afc4b8c
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-05-27 09:05:22 +0600

    Created global error handler utils

---
+++ src/app/utils/globalErrorHandler/globalErrorHandler.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from 'express';
+
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  return res.status(500).json({
+    success: false,
+    message: 'Something went wrong!!',
+    error: err.message,
+  });
+};
+
+export default globalErrorHandler;

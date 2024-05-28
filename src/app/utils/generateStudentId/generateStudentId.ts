e896db9070ff76e792fba2ddbfd715e4e8e4fe8c
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-05-28 09:28:10 +0600

    Created an utility function to dynamically generate student ID

---
+++ src/app/utils/generateStudentId/generateStudentId.ts
@@ -0,0 +1,30 @@
+import { TAcademicSemester } from '../../modules/academicSemeter/academicSemeter.iterface';
+import UserModel from '../../modules/user/user.model';
+
+const findLastStudentId = async () => {
+  const lastStudent = await UserModel.findOne(
+    {
+      role: 'student',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+};
+
+export const generateStudentId = async (payload: TAcademicSemester) => {
+  const currentId = (await findLastStudentId()) || (0).toString();
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `${payload.year}${payload.code}${incrementId}`;
+
+  return incrementId;
+};

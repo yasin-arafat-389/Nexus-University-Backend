81d1b317da940bf535b44b287c4c27cf11573dd2
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-05-29 13:17:27 +0600

    Added transaction and rollback for creating student

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,23 +1,56 @@
+import mongoose from 'mongoose';
 import config from '../../config';
+import { generateStudentId } from '../../utils/generateStudentId/generateStudentId';
+import AcademicSemesterModel from '../academicSemeter/academicSemeter.model';
 import { TStudent } from '../student/student.interface';
-import StudentModel from '../student/student.model';
 import { TUser } from './user.interface';
+import { TAcademicSemester } from '../academicSemeter/academicSemeter.iterface';
 import UserModel from './user.model';
+import StudentModel from '../student/student.model';
 
-const createStudent = async (password: string, studentData: TStudent) => {
-  const user: Partial<TUser> = {};
+const createStudent = async (password: string, payload: TStudent) => {
+  const userData: Partial<TUser> = {};
 
-  user.password = password || (config.default_pass as string);
-  user.role = 'student';
-  user.id = '2030010001';
+  userData.password = password || (config.default_pass as string);
 
-  const newUser = await UserModel.create(user);
+  userData.role = 'student';
 
-  if (newUser) {
-    studentData.id = newUser.id;
-    studentData.user = newUser._id;
-    const newStudent = StudentModel.create(studentData);
+  const admissionSemester = await AcademicSemesterModel.findById(
+    payload.admissionSemester,
+  );
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    userData.id = await generateStudentId(
+      admissionSemester as TAcademicSemester,
+    );
+
+    const newUser = await UserModel.create([userData], { session }); // array
+
+    if (!newUser.length) {
+      throw new Error('Failed to create user!');
+    }
+
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id;
+
+    const newStudent = await StudentModel.create([payload], { session });
+
+    if (!newStudent.length) {
+      throw new Error('Failed to create student!');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error('Failed to create student');
   }
 };
 

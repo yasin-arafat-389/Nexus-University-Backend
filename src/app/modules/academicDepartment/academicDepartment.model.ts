193db94f6b1c9ae682ecd2936aedd65d787eb231
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-05-31 15:32:39 +0600

    Designed academic department model

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose';
+import { TAcademicDepartment } from './academicDepartment.interface';
+
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: 'ObjectID',
+      ref: 'AcademicFaculty',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartmentModel.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new Error('This department already exists!');
+  }
+
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartmentModel.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new Error('This department does not exist');
+  }
+
+  next();
+});
+
+export const AcademicDepartmentModel = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);

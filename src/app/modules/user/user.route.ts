de6cd91f9ed65d5a43eac8ccede76cf716fb0ca6
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-02 19:59:55 +0600

    created a route to create new faculty member

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { UserControllers } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest/validateRequest';
 import { validateStudentRequest } from '../student/student.validation';
+import { validateFacultyMember } from '../facultyMember/facultyMember.validation';
 
 const router = express.Router();
 
@@ -9,6 +10,12 @@ router.post(
   '/create-student',
   validateRequest(validateStudentRequest.studentValidation),
   UserControllers.createStudent,
+);
+
+router.post(
+  '/create-faculty-member',
+  validateRequest(validateFacultyMember.facultyMemberValidation),
+  UserControllers.createFacultyMembers,
 );
 
 export const UserRoutes = router;

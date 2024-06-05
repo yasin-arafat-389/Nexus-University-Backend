17e6a34576631d7a69aa0b83c27f06fb18de9328
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-05 10:15:02 +0600

    Added update validation schema for faculty member

--- src/app/modules/facultyMember/facultyMember.validation.ts
+++ src/app/modules/facultyMember/facultyMember.validation.ts
@@ -27,6 +27,31 @@ export const facultyMemberValidation = z.object({
   }),
 });
 
+const facultyMemberUpdateValidation = z.object({
+  designation: z.string().optional(),
+  name: z
+    .object({
+      firstName: z.string().optional(),
+      middleName: z.string().optional(),
+      lastName: z.string().optional(),
+    })
+    .optional(),
+  gender: z.enum(['male', 'female', 'other']).optional(),
+  dateOfBirth: z.string().optional(),
+  email: z.string().email().optional(),
+  contactNo: z.string().optional(),
+  emergencyContactNo: z.string().optional(),
+  bloogGroup: z
+    .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
+    .optional(),
+  presentAddress: z.string().optional(),
+  permanentAddress: z.string().optional(),
+  profileImg: z.string().optional(),
+  academicDepartment: z.string().optional(),
+  isDeleted: z.boolean().optional(),
+});
+
 export const validateFacultyMember = {
   facultyMemberValidation,
+  facultyMemberUpdateValidation,
 };

b7b72813176a1e32d5b80bea03f9c7df3358cf4c
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-05 10:13:53 +0600

    Created all services for faculty members

--- src/app/modules/facultyMember/facultyMember.service.ts
+++ src/app/modules/facultyMember/facultyMember.service.ts
@@ -1,13 +1,68 @@
-import FacultyMemberModel from './facultyMember.model';
+import onFailure from '../../utils/onFailure/onFailure';
+import { TFacultyMember } from './facultyMember.interface';
+import { FacultyMemberModel } from './facultyMember.model';
 
 const GetAllFacultyMember = async () => {
   const result = await FacultyMemberModel.find()
     .populate('user')
     .populate('academicDepartment');
+  return result;
+};
 
+const GetSingleFacultyMember = async (id: string) => {
+  const result = await FacultyMemberModel.findOne({ id })
+    .populate('user')
+    .populate('academicDepartment');
+  return result;
+};
+
+const UpdateFacultyMember = async (
+  id: string,
+  payload: Partial<TFacultyMember>,
+) => {
+  const { name, ...remainingFacultyMemberData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingFacultyMemberData,
+  };
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+
+  console.log(id);
+  console.log(payload);
+
+  const result = await FacultyMemberModel.findOneAndUpdate(
+    { id },
+    modifiedUpdatedData,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  onFailure(result, 'ID not found!!');
+  return result;
+};
+
+const DeleteFacultyMember = async (id: string) => {
+  const result = await FacultyMemberModel.findOneAndUpdate(
+    { id },
+    { isDeleted: true },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
   return result;
 };
 
 export const FacultyMemberServices = {
   GetAllFacultyMember,
+  GetSingleFacultyMember,
+  UpdateFacultyMember,
+  DeleteFacultyMember,
 };

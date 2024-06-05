76844bc2f1c1ef2dbd4d0474d9107a31489c48f8
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-05 10:15:52 +0600

    Created all controllers for faculty members

--- src/app/modules/facultyMember/facultyMember.controller.ts
+++ src/app/modules/facultyMember/facultyMember.controller.ts
@@ -12,6 +12,46 @@ const getAllFacultyMembers: RequestHandler = async (req, res, next) => {
   }
 };
 
+const getSingleFacultyMembers: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await FacultyMemberServices.GetSingleFacultyMember(
+      req.params.id,
+    );
+
+    sendResponse(res, result, 'Faculty Member fetched succesfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateFacultyMembers: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await FacultyMemberServices.UpdateFacultyMember(
+      req.params.id,
+      req.body,
+    );
+
+    sendResponse(res, result, 'Faculty Member updated succesfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteFacultyMembers: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await FacultyMemberServices.DeleteFacultyMember(
+      req.params.id,
+    );
+
+    sendResponse(res, result, 'Faculty Member deleted succesfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const FacultyMemberControllers = {
   getAllFacultyMembers,
+  getSingleFacultyMembers,
+  updateFacultyMembers,
+  deleteFacultyMembers,
 };

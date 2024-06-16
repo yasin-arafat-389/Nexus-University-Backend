2105253ae030b0894eadff81d9a7d6d0db2cb92d
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-16 08:02:00 +0600

    Added all the application routes to index

---
+++ src/app/routes/index.ts
@@ -0,0 +1,64 @@
+import { Router } from 'express';
+import { UserRoutes } from '../modules/user/user.route';
+import { StudentRoutes } from '../modules/student/student.route';
+import { AcademicSemesterRoutes } from '../modules/academicSemeter/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
+import { FacultyMemberRoutes } from '../modules/facultyMember/facultyMember.route';
+import { AdminRoutes } from '../modules/Admin/admin.route';
+import { SemesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route';
+import { OfferedCourseRoutes } from '../modules/offeredCourse/offeredCourse.route';
+import { AuthRoutes } from '../modules/auth/auth.route';
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: '/user',
+    route: UserRoutes,
+  },
+  {
+    path: '/student',
+    route: StudentRoutes,
+  },
+  {
+    path: '/academic-semester',
+    route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculty',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-department',
+    route: AcademicDepartmentRoutes,
+  },
+  {
+    path: '/faculty-member',
+    route: FacultyMemberRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes,
+  },
+  {
+    path: '/admin',
+    route: AdminRoutes,
+  },
+  {
+    path: '/semester-registrations',
+    route: SemesterRegistrationRoutes,
+  },
+  {
+    path: '/offered-courses',
+    route: OfferedCourseRoutes,
+  },
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;

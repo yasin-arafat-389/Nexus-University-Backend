4bbb361715d4e4b83d4c46a72b05ee5b7030f804
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-21 10:43:42 +0600

    Created an utility to upload image to cloudinary

---
+++ src/app/utils/uploadImageToCloudinary/uploadImageToCloudinary.ts
@@ -0,0 +1,49 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import multer from 'multer';
+import config from '../../config';
+
+export type TCloudinaryResponse = {
+  secure_url: string;
+};
+
+cloudinary.config({
+  cloud_name: config.cloudinary_cloud_name,
+  api_key: config.cloudinary_api_key,
+  api_secret: config.cloudinary_api_secret,
+});
+
+export const uploadImageToCloudinary = (imageName: string, path: string) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      path,
+      { public_id: imageName },
+      function (error, result) {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+        // delete a file asynchronously
+        fs.unlink(path, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('File has been deleted.');
+          }
+        });
+      },
+    );
+  });
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, process.cwd() + '/uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
+
+export const upload = multer({ storage: storage });

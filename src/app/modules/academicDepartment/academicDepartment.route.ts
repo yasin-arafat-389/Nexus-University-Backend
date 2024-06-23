8d81f4065346e5b6888694a3a683ce9c0871ddb2
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-23 09:05:29 +0600

    Created academic department routes

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { AcademicDepartmentControllers } from './academicDepartment.controller';
+import { AcademicDepartmentValidation } from './academicDepartment.validation';
+import validateRequest from '../../middlewares/validateRequest/validateRequest';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartmemt,
+);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartment,
+);
+
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDeartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+export const AcademicDepartmentRoutes = router;

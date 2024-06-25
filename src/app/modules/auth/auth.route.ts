bc56fb5956a466ff6b90737fdbe3c1b9a914aad2
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-25 11:59:25 +0600

    Created auth routes

---
+++ src/app/modules/auth/auth.route.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import { AuthControllers } from './auth.controller';
+import validateRequest from '../../middlewares/validateRequest/validateRequest';
+import { AuthValidation } from './auth.validation';
+
+const router = express.Router();
+
+router.post(
+  '/login',
+  validateRequest(AuthValidation.loginValidationSchema),
+  AuthControllers.login,
+);
+
+router.post(
+  '/refresh-token',
+  validateRequest(AuthValidation.refreshTokenValidationSchema),
+  AuthControllers.refreshToken,
+);
+
+router.post(
+  '/forget-password',
+  validateRequest(AuthValidation.forgetPasswordValidationSchema),
+  AuthControllers.forgetPassword,
+);
+
+router.post(
+  '/reset-password',
+  validateRequest(AuthValidation.resetPasswordValidationSchema),
+  AuthControllers.resetPassword,
+);
+
+export const AuthRoutes = router;

96477099d7556a300a02ed13c6a61fe6aa5c41b6
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-30 09:43:03 +0600

    created user routes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,25 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UserControllers } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest/validateRequest';
 import { validateStudentRequest } from '../student/student.validation';
 import { validateFacultyMember } from '../facultyMember/facultyMember.validation';
+import { validateAdmin } from '../Admin/admin.validation';
+import { upload } from '../../utils/uploadImageToCloudinary/uploadImageToCloudinary';
 
 const router = express.Router();
 
 router.post(
   '/create-student',
+
+  upload.single('file'),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
+
   validateRequest(validateStudentRequest.studentValidation),
+
   UserControllers.createStudent,
 );
 
@@ -16,6 +27,12 @@ router.post(
   '/create-faculty-member',
   validateRequest(validateFacultyMember.facultyMemberValidation),
   UserControllers.createFacultyMembers,
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(validateAdmin.AdminValidation),
+  UserControllers.createAdmin,
 );
 
 export const UserRoutes = router;
